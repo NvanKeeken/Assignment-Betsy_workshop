635e1e5dbafcf8164ba49af816a27004f16b5a1b
N van Keeken <n.v.k20@gmail.com>
2023-07-06 19:13:33 +0200

    added BaseModel

--- models.py
+++ models.py
@@ -1,1 +1,29 @@
 # Models go here 
+from peewee import (Model, 
+                    SqliteDatabase, 
+                    CharField, 
+                    ForeignKeyField, 
+                    IntegerField, 
+                    FloatField)
+
+db = SqliteDatabase("betsy.db")
+
+class BaseModel(Model):
+
+    class Meta:
+        database = db
+
+class User(BaseModel):
+  pass
+
+class Product(BaseModel):
+   pass
+
+class Tag():
+   pass
+
+class ProductTag():
+   pass
+
+class Transactions():
+   pass
