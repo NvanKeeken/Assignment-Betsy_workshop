18989629aee2085cf967dcd180cd9e486f07a1eb
N van Keeken <n.v.k20@gmail.com>
2023-07-07 13:38:26 +0200

    added delete db function

---
+++ setupdb.py
@@ -0,0 +1,9 @@
+from models import User, Product, Transactions, Tag, ProductTag
+import os
+
+def delete_database():
+    cwd = os.getcwd()
+    database_path = os.path.join(cwd, "betsy.db")
+    if os.path.exists(database_path):
+        os.remove(database_path)
+
