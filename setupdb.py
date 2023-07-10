4ca1f9ac23df5158c3334806e6ca0ba137f30797
N van Keeken <n.v.k20@gmail.com>
2023-07-10 14:53:04 +0200

    added User test_data

--- setupdb.py
+++ setupdb.py
@@ -19,7 +19,27 @@ def set_test_data():
     ])
 
     # define test data per table
-    users = []
+    users = [
+              ["Louise", "Hendriks", "Bekerbaan", 
+              "4", "6333 EZ", "Schimmert", 
+              "The Netherlands", "457868"],
+
+              ["Denis", "Jansen", "Hanewei", 
+               "8a", "6344 GH", "Schinnen",
+               "The Netherlands", "567897"],
+
+               ["Abel", "Oliver", "Baker Street", 
+                "11", "NW1", "London", 
+                "UK", "678967"],
+
+                ["Jenny", "Patterson", "Cannon Street", 
+                 "12", "B2 5EP", "Birmingham", 
+                 "UK", "56789"],
+
+                 ["Helena", "Joosten", "Hoofdstraat", 
+                  "62", "7311 At", "Apeldoorn", 
+                  "The Netherlands", "467390"]
+               ]
 
     products = []
 
@@ -30,3 +50,13 @@ def set_test_data():
     product_tags = []
 
     # Insert new data per table
+    for user in users:
+        firstname, lastname, street, street_nr, postal_code, city, country, account_nr = user
+        User.create(first_name = firstname, 
+                    last_name = lastname,
+                    street_name = street,
+                    house_number = street_nr,
+                    postal_code = postal_code,
+                    city = city,
+                    country = country,
+                    bank_account_number = account_nr)
